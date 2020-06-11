4ad7054a63b5772f44beba4643e262a0a0ba1dbd
Dario Zubillaga <dariozubi@gmail.com>
2020-06-11 13:40:41 -0500

    Added gabrielazubillaga to works

---
+++ pages/en/work/gabrielazubillaga.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import Router from 'next/router'
+
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+import Grow from '@material-ui/core/Grow';
+import Fade from '@material-ui/core/Fade';
+import Fab from '@material-ui/core/Fab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Layout from '../../../src/util/Layout';
+import banner from '../../../images/gaby-banner.jpg';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    maxWidth: '100%'
+  },
+  media: {
+    height: 164,
+  },
+  fab: {
+    position: 'absolute',
+    right: theme.spacing(6),
+    marginBottom: theme.spacing(4)
+  }
+}));
+
+function Text(){
+  const classes = useStyles();
+  const [fade, setFade] = useState(false);
+
+  useEffect(() => {
+    setFade(true);
+  }, [])
+
+  return(
+    <React.Fragment>
+
+      <Box mt={8}>
+        <Fade in={fade} timeout={800}>
+          <Card className={classes.root} elevation={0}>
+              <CardMedia
+                className={classes.media}
+                image={banner}
+              />
+          </Card>
+        </Fade>
+      </Box>
+
+      <Box mx={4} my={4}>
+
+        <Grow in={fade} timeout={850}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Gabriela Zubillaga
+          </Typography>
+        </Grow>
+
+        <Grow in={fade} timeout={900}>
+          <p>Artist Gabriela Zubillaga asked me to make her <a href="https://gabrielazubillaga.com/" target="_blank" rel="noopener">website</a>. Since she wanted to make edits by herself and add a shop later, I recommended to make it in Wordpress since it's friendly with non-technical users.</p> 
+        </Grow>
+
+        <Grow in={fade} timeout={950}>
+          <Fab size="small" color="primary" aria-label="back" className={classes.fab} onClick={()=>Router.back()}>
+            <ArrowBackIcon />
+          </Fab>
+        </Grow>
+
+      </Box>
+
+    </React.Fragment>
+  )
+}
+
+export default function Gaby() {
+  return (
+    <Layout
+      title="Gabriela Zubillaga"
+      description="Página web de la artista Gabriela Zubillaga"
+    >
+      <Text/>
+    </Layout>
+  )
+}
--- src/work/data.js
+++ src/work/data.js
@@ -6,6 +6,7 @@ import inbursa from '../../images/inbursa.jpg';
 import oakley from '../../images/oakley.jpg';
 import mazic from '../../images/mazic-logo.jpg';
 import pame from '../../images/pame.jpg';
+import gaby from '../../images/gaby.jpg';
 import photobooth from '../../images/photobooth.jpg';
 import porsche from '../../images/porsche.jpg';
 import sumo from '../../images/sumo-logo.jpg';
@@ -13,27 +14,29 @@ import unifin from '../../images/unifin.jpg';
 import argax from '../../images/argax.jpg';
 
 export default [
-  
+
   {
     css:
-      'url('+pame+')',
+      'url('+cps+')',
     height: 400,
-    type: 'web',
-    title: 'Pamela Zubillaga',
-    description: 'Wordpress portfolio',
-    descripcion: 'Página de Wordpress',
-    link: 'pamelazubillaga'
+    type: 'more',
+    title: 'CPS',
+    description: 'Form development for CPS',
+    descripcion: 'Desarrollo de formularios para CPS',
+    link: 'CPS'
   },
+
   {
     css:
-      'url('+hoxa+')',
+      'url('+argax+')',
     height: 500,
-    type: 'web',
-    title: 'hoxa',
-    description: 'Wordpress e-commerce website',
-    descripcion: 'Tienda virtual hecha con Wordpress',
-    link: 'hoxa'
+    type: 'vr',
+    title: 'Árgax',
+    description: 'VR app for 3D music composing',
+    descripcion: 'Aplicación en RV para música 3D',
+    link: 'argax'
   },
+
   {
     css:
       'url('+dazuma+')',
@@ -44,56 +47,38 @@ export default [
     descripcion: 'Página personal hecha con NextJS',
     link: 'dazuma'
   },
+    
+  {
+    css:
+      'url('+gaby+')',
+    height: 350,
+    type: 'web',
+    title: 'Gabriela Zubillaga',
+    description: 'Wordpress portfolio',
+    descripcion: 'Página de Wordpress',
+    link: 'gabrielazubillaga'
+  },
 
   {
     css:
-      'url('+inbursa+')',
+      'url('+hoxa+')',
+    height: 500,
+    type: 'web',
+    title: 'hoxa',
+    description: 'Wordpress e-commerce website',
+    descripcion: 'Tienda virtual hecha con Wordpress',
+    link: 'hoxa'
+  },
+
+  {
+    css:
+      'url('+pame+')',
     height: 400,
-    type: 'multimedia',
-    title: 'Inbursa',
-    description: 'Painting canvas and augmented reality',
-    descripcion: 'Dibujos virtuales y realidad aumentada',
-    link: 'inbursa'
-  },
-  {
-    css:
-      'url('+oakley+')',
-    height: 450,
-    type: 'multimedia',
-    title: 'Oakley',
-    description: 'Video based virtual race',
-    descripcion: 'Carrera virtual basada en video',
-    link: 'oakley'
-  },
-  {
-    css:
-      'url('+porsche+')',
-    height: 350,
-    type: 'multimedia',
-    title: 'Porsche',
-    description: 'Videowall',
-    descripcion: 'Videowall',
-    link: 'porsche'
-  },
-  {
-    css:
-      'url('+unifin+')',
-    height: 400,
-    type: 'multimedia',
-    title: 'Unifin',
-    description: 'Twitter controlled cars',
-    descripcion: 'Carritos controlados por tweets',
-    link: 'unifin'
-  },
-  {
-    css:
-      'url('+photobooth+')',
-    height: 450,
-    type: 'multimedia',
-    title: 'photobooth',
-    description: 'Digital photobooth',
-    descripcion: 'Photobooth interactivo digital',
-    link: 'photobooth'
+    type: 'web',
+    title: 'Pamela Zubillaga',
+    description: 'Wordpress portfolio',
+    descripcion: 'Página de Wordpress',
+    link: 'pamelazubillaga'
   },
 
   {
@@ -106,28 +91,63 @@ export default [
     descripcion: 'Reacreacion de un comercial en RV',
     link: 'givenchy'
   },
+  
+  
   {
     css:
-      'url('+argax+')',
-    height: 500,
-    type: 'vr',
-    title: 'Árgax',
-    description: 'VR app for 3D music composing',
-    descripcion: 'Aplicación en RV para música 3D',
-    link: 'argax'
+      'url('+oakley+')',
+    height: 450,
+    type: 'multimedia',
+    title: 'Oakley',
+    description: 'Video based virtual race',
+    descripcion: 'Carrera virtual basada en video',
+    link: 'oakley'
   },
 
-
   {
     css:
-      'url('+cps+')',
+      'url('+inbursa+')',
     height: 400,
-    type: 'more',
-    title: 'CPS',
-    description: 'Form development for CPS',
-    descripcion: 'Desarrollo de formularios para CPS',
-    link: 'CPS'
+    type: 'multimedia',
+    title: 'Inbursa',
+    description: 'Painting canvas and augmented reality',
+    descripcion: 'Dibujos virtuales y realidad aumentada',
+    link: 'inbursa'
   },
+
+  {
+    css:
+      'url('+porsche+')',
+    height: 350,
+    type: 'multimedia',
+    title: 'Porsche',
+    description: 'Videowall',
+    descripcion: 'Videowall',
+    link: 'porsche'
+  },
+
+  {
+    css:
+      'url('+unifin+')',
+    height: 400,
+    type: 'multimedia',
+    title: 'Unifin',
+    description: 'Twitter controlled cars',
+    descripcion: 'Carritos controlados por tweets',
+    link: 'unifin'
+  },
+
+  {
+    css:
+      'url('+photobooth+')',
+    height: 450,
+    type: 'multimedia',
+    title: 'photobooth',
+    description: 'Digital photobooth',
+    descripcion: 'Photobooth interactivo digital',
+    link: 'photobooth'
+  },
+  
   {
     css:
       'url('+sumo+')',
@@ -138,6 +158,7 @@ export default [
     descripcion: 'Simulaciones de tráfico y semáforos',
     link: 'SUMO'
   },
+  
   {
     css:
       'url('+mazic+')',
